13df8763668208ed1b3ec5e29e5e9136085176fa
Muhamad Mustafa Babakr <106625489+IYawar@users.noreply.github.com>
2022-10-14 05:21:11 +0300

    Been testing with api and js today, nothing special.

--- java.js
+++ java.js
@@ -13,3 +13,14 @@ window.onclick = function(event) {
         }
     }
 }
+
+async function getdata(){
+    const response = await fetch("https://a.vibot.tech:3002/api/afkchecks")
+    const data = await response.json()
+    
+    document.getElementById('afk-test').textContent = afk
+}
+getdata()
+
+setInterval(getdata, 3000)
+console.log(getdata())
