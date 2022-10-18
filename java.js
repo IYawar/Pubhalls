a63d53971fe0e318ab33bb77c2bde855cd0ec389
Muhamad Mustafa Babakr <106625489+IYawar@users.noreply.github.com>
2022-10-18 04:23:35 +0300

    Testing with afk checks still

--- java.js
+++ java.js
@@ -24,19 +24,24 @@ getdata()
 window.onload = async (event) => {
     let activeAfk = [] ,activeRun = []
     let data = await getdata()
+    console.log(data)
     for(let i in data){
         if (data[i].active) activeRun.push(data[i])
         else activeAfk.push(data[i])
     }
-
+    let leader = data.leaderNick
     function RenderAfk (afkCheck, active){
         let started
+        let rectang = document.createElement("div")
+        let afk = document.getElementById("pend")
         if (afkCheck.started) started = Math.abs((Data.now() - afkCheck.started) / 1000)
-        let rectang = document.createElement('div')
-        if (active) return {
-
+        if (active) {
+            rectang.innerHTML = "<strong3>Ayo mr.White check this out</strong>"
+            afk.append(rectang)
+            console.log(leader)
         }
-    }
-    console.log(data)
+    
 }
-
+setInterval(RenderAfk(data, true), 3000)
+setInterval(RenderAfk(data, false), 3000)
+}
