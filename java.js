9dabb42f28fe46c25cd8d7861cfd8b975e918303
Muhamad Mustafa Babakr <106625489+IYawar@users.noreply.github.com>
2022-10-26 06:11:47 +0300

    trying to optimize afk still..

--- java.js
+++ java.js
@@ -21,30 +21,51 @@ async function getdata() {
 }
 
 window.onload = leggo
+let rectang = document.createElement("div")
+let afk = document.getElementById("pend")
+let run = document.getElementById("run")
+let boxes = document.getElementsByClassName("rbox")
 async function leggo() {
     let activeAfk = [],activeRun = []
     let data = await getdata()
+    
     console.log(data)
     for (let i of Object.values(data)) {
         if (i.active) activeRun.push(i)
         else activeAfk.push(i)
-        RenderAfk(i, i.active)
+        activeAfk.map(afk => RenderAfk(i, i.active))
+        activeRun.map(afk => RenderAfk(i, i.false))
+        //RenderAfk(i, i.active)
         console.log(i.leaderNick)
         
     }
-}
-let rectang = document.createElement("div")
-let afk = document.getElementById("pend")
-let run = document.getElementById("run")
 
-setInterval(leggo, 3000)
+
+
 function RenderAfk(afkCheck, active) {
-    delete rectang
+ 
     if (active)  {
-        rectang.innerHTML = `<a href="${afkCheck.url} target="_blank" rel='noopener noreferrer'"><div class="rbox"><p class="left">${afkCheck.runType.runName} by ${afkCheck.leaderNick}</p><p class="mid">6:00</p><p class="right">30/45</p></div></a>`
+        rectang.innerHTML = `<a href="${afkCheck.url}"" target="_blank"><div class="rbox"><p class="left">${afkCheck.runType.runName} by ${afkCheck.leaderNick}</p><p class="mid">6:00</p><p class="right">${afkCheck.vcSize || '0'}/${afkCheck.runType.vcCap}</p></div></a>`
         afk.append(rectang)
     } else {
-        rectang.innerHTML = `<a href="${afkCheck.url} target="_blank" rel='noopener noreferrer'><div class="rbox"><p class="left">${afkCheck.runType.runName} by ${afkCheck.leaderNick}</p><p class="mid">6:00</p><p class="right">30/45</p></div></a>`;
+        rectang.innerHTML = `<a href="${afkCheck.url}" target="_blank"><div class="rbox"><p class="left">${afkCheck.runType.runName} by ${afkCheck.leaderNick}</p><p class="mid">xx:xx</p><p class="right">${afkCheck.vcSize || '0'}/${afkCheck.runType.vcCap}</p></div></a>`;
         run.append(rectang)
     }
 }
+}setInterval(leggo, 3000)
+/*if (data && Object.keys(data).length) {
+    activeAfk.map(afk => RenderAfk(afk, true))
+    activeRun.map(afk => RenderAfk(afk, false))
+}
+}setInterval(leggo, 3000)
+*/
+/*function RenderAfk(afkCheck, active) {
+    for (let b = 0 ; b <= afkCheck.length; b++){
+     if (afkCheck[b].active)  {
+         rectang.innerHTML = `<a href="${afkCheck.url}"" target="_blank"><div class="rbox"><p class="left">${afkCheck.runType.runName} by ${afkCheck.leaderNick}</p><p class="mid">6:00</p><p class="right">${afkCheck.vcSize || '0'}/45</p></div></a>`
+         afk.append(rectang)
+     } else if (afkCheck[b].active === false){
+         rectang.innerHTML = `<a href="${afkCheck.url}" target="_blank"><div class="rbox"><p class="left">${afkCheck.runType.runName} by ${afkCheck.leaderNick}</p><p class="mid">xx:xx</p><p class="right">${afkCheck.vcSize || '0'}/45</p></div></a>`;
+         run.append(rectang)
+     }
+     } */
