4ee7683df97edc3562ed5f576fa2a75508025189
Muhamad Mustafa Babakr <106625489+IYawar@users.noreply.github.com>
2022-11-19 01:04:10 +0300

    Making info for site embed

--- public/java.js
+++ public/java.js
@@ -34,10 +34,8 @@ async function leggo() {
     
     for (let i of Object.values(data)) {
         if ( i.leader != null){
-        if (i.runType.embed.color != []){
         if (i.active === false) activeRun.push(i)
         else activeAfk.push(i)
-        }
     }}
      afk.innerHTML = ""
      safk.innerHTML = ""
